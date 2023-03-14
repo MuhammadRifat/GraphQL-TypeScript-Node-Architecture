83e381fb726da4f1b637f24688ca47f6a89f7d6c
raihan50017 <aburaihan50017@gmail.com>
2023-03-14 10:00:04 +0600

    14/03/2023

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,100 @@ app.get('/', (req: Request, res: Response) => {
     });
 });
 
+app.get('/academic-journal', (req: Request, res: Response) => {
+    return res.render("academic-journal", {
+        title: "Journal"
+    });
+});
+
+
+app.get('/journal-details', (req: Request, res: Response) => {
+    return res.render("journal-details", {
+        title: "Journal"
+    });
+});
+
+
+app.get('/article-details', (req: Request, res: Response) => {
+    return res.render("article-details", {
+        title: "Journal"
+    });
+});
+
+app.get('/authors-information', (req: Request, res: Response) => {
+    return res.render("authors-information", {
+        title: "Journal"
+    });
+});
+
+app.get('/blog', (req: Request, res: Response) => {
+    return res.render("blog", {
+        title: "Journal"
+    });
+});
+
+app.get('/contact', (req: Request, res: Response) => {
+    return res.render("contact", {
+        title: "Journal"
+    });
+});
+
+app.get('/contact-journal', (req: Request, res: Response) => {
+    return res.render("contact-journal", {
+        title: "Journal"
+    });
+});
+
+
+app.get('/current-issue', (req: Request, res: Response) => {
+    return res.render("current-issue", {
+        title: "Journal"
+    });
+});
+
+app.get('/volume-issue', (req: Request, res: Response) => {
+    return res.render("volume-issue", {
+        title: "Journal"
+    });
+});
+
+app.get('/editorial-board', (req: Request, res: Response) => {
+    return res.render("editorial-board", {
+        title: "Journal"
+    });
+});
+
+app.get('/information', (req: Request, res: Response) => {
+    return res.render("information", {
+        title: "Journal"
+    });
+});
+
+app.get('/login', (req: Request, res: Response) => {
+    return res.render("login", {
+        title: "Journal"
+    });
+});
+
+app.get('/news', (req: Request, res: Response) => {
+    return res.render("news", {
+        title: "Journal"
+    });
+});
+
+app.get('/news-details', (req: Request, res: Response) => {
+    return res.render("news-details", {
+        title: "Journal"
+    });
+});
+
+app.get('/registration', (req: Request, res: Response) => {
+    return res.render("registration", {
+        title: "Journal"
+    });
+});
+
+
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
     logger.error("Route not found!");
     return res.render("404");
